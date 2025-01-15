8e603be76b2c3790462aa44b65096b3cc9170f47
Justin Paul <justin@jpaul.me>
2025-01-14 19:57:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import time
 print("Running main application...")
 
 while True:
-    print("Device is operational with 2.0 code!")
+    print("Device is operational with 3.0 code!")
     time.sleep(5)
