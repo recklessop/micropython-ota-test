e45e282ccdba9d6a027d4453ffc56631fdbe0da4
Justin Paul <justin@jpaul.me>
2025-01-14 20:05:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ print("Running main application...")
 while True:
     print("Device is operational with 3.0 code!")
     time.sleep(5)
+
+# In main.py
+with open("log.txt", "a") as log:
+    log.write("Device started successfully.\n")
+    log.write("Running main program...\n")
